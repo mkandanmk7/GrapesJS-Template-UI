5b2f6e022e13947f58f2971cfda9215946241b02
mkandanmk7 <mkandanmk7@gmail.com>
2022-02-21 12:54:09 +0530

    Changes

--- src/Components/myBlocks.js
+++ src/Components/myBlocks.js
@@ -1,40 +1,116 @@
 import { map } from "lodash";
 
 export const myBlocks = (editor) => {
+
+    const $ = editor.$;
+
     console.log("editor:", editor);
+    let selectedCompId;
     editor.on('component:selected', component => {
-        console.log("component:",component)
-        let selectedComp = editor.getSelected();
-        console.log(selectedComp)
-        let selectedCompId=selectedComp.attributes.attributes.id 
-        if(selectedCompId === "iw5f"){
-            console.log("id :",selectedCompId)
-            let toolbar=selectedComp.attributes.toolbar;
-            console.log('toolbar', toolbar)
-            let setClass=map(toolbar,(val,index)=>{
-                val.attributes.class={display:"none"}
-                // console.log("value:",val)
+
+        // const newTool = {
+        //     icon: "fa fa-plus-square",
+        //     title: "check Toolbar",
+        //     command: "",
+        //     id:""
+        // };
+        // const defaultToolbar = component.get("toolbar");
+        // const isToolbarExist = defaultToolbar.filter(
+        //     (tool) => tool.command === newTool.command);
+
+        //     if(!isToolbarExist){
+        //         defaultToolbar.unshift({
+        //             id:newTool.id,
+        //             attributes:{class:newTool.icon,title:newTool.title},
+        //             command:newTool.command,
+        //         })
+        //     }
+
+
+         selectedCompId = component.attributes.attributes.id
+
+        const hanldeToolbar = (value) => {
+            $('.gjs-toolbar').each(function () {
+                this.style.right = value;
             })
         }
-        else return console.log("selected comp is not wrapper comp")
+
+        const isNotWrapper = (value) => {
+            $('.gjs-toolbar').each(function () {
+                this.style.right = value;
+            })
+        }
+
+        if (selectedCompId === "iwgn") {
+            hanldeToolbar("100000px")
+        }
+        else {
+            isNotWrapper("auto")
+        }
     });
+
+    const keyCheck = (event) => {
+        console.log("in keyCheck")
+        let deleteKey = 46;
+        console.log(event.keyCode)
+        if (event.keyCode === deleteKey) {
+
+            event.preventDefault();
+            window.confirm("dont delete")
+           
+
+        }
+        else {
+            console.log("not delete key")
+        }
+    }
+   
+    editor.on("component:remove", (component) => {
+        
+
+        console.log("component in remove", component)
+       let selectedCompoId = selectedCompId;
+       console.log(selectedCompoId)
+        if (selectedCompoId === "iwgn") {
+
+            console.log("wrapper comp cant delete")
+            document.onkeydown = keyCheck;
+        }
+        else {
+            console.log("in else")
+            // document.addEventListener("keydown", keyCheck);
+
+        }
+
+    })
+
+    // getToolbar.style.display="none";
+    //   .gjs-toolbar {dispaly:"none"}
+    // console.log("id :", selectedCompId)
+    // let toolbar = component.attributes.toolbar;
+    // console.log('toolbar', toolbar)
+    // let setClass = map(toolbar, (val, index) => {
+    //     val.attributes.class = { display: "none" }
+    //     // console.log("value:",val)
+    // })
+
 
     // get components
     // let comp=editor.Components;
     // let wrapper=comp.getWrapper();
     // wrapper.set("style",{"background-color":"green"});
 
-    editor.on("component:add", () => {
-        return {
-            tagName: "div",
-            removable: true,
-            draggable: true,
-            copyable: true,
-            content: `<div><h3>Hello World ...!</h3><button>Submit</button></div>`,
-            style: { color: "green" },
-            attributes: { title: "here" }
-        }
-    })
+    // editor.on("component:add", () => {
+    //     return {
+    //         tagName: "div",
+    //         removable: true,
+    //         draggable: true,
+    //         copyable: true,
+    //         content: `<div><h3>Hello World ...!</h3><button>Submit</button></div>`,
+    //         style: { color: "green" },
+    //         attributes: { title: "here" }
+    //     }
+    // })
 
     // let newComp=comp.addComponent({
     //     tagName:"div",
@@ -50,7 +126,7 @@ export const myBlocks = (editor) => {
     // iw5f
     // editor.on("component:update", () => {
 
-    //     selectedComp.attributes.toolbar[3].attributes.class = { display: "none" }
+    //     component.attributes.toolbar[3].attributes.class = { display: "none" }
     // })
 
 
@@ -77,4 +153,9 @@ export const myBlocks = (editor) => {
     // }
     //   })
 }
+
+
+
+
+
 
